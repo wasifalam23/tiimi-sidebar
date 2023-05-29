87778bec2a658b7ab43c35416c150d453802ed06
wasif_alam <wsfalam84@gmail.com>
2023-05-29 18:57:22 +0530

    added sidebar with complete styles

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <SideBar />
+
+    <div class="p-0 sm:ml-64">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
---
+++ pages/inbox.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h3>This is the inbox page</h3>
+  </div>
+</template>
